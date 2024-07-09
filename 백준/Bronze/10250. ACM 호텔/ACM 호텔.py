8d213e030b02cd77faccb4af171bb84786e9341c
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-09 15:12:11 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,9 @@
+T = int(input())
+for i in range(T):
+    h, w, n = map(int, input().split())
+    floor = n % h
+    room = (n // h) + 1
+    if floor == 0:
+        floor = h
+        room -= 1
+    print(floor*100 + room)
