d07d4a1601f7d0bffafd49c4b47f8578604c53b5
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-09 16:03:09 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+scales = list(map(int, input().split()))
+if scales == sorted(scales):
+    print("ascending")
+elif scales == sorted(scales, reverse=True):
+    print("descending")
+else:
+    print("mixed")
