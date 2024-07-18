49eaa8b0e285e5054dae0e9b775594350e9eede7
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-18 21:49:27 +0900

    [Silver III] Title: 통계학, Time: 488 ms, Memory: 53516 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -8,21 +8,12 @@ print(round(sum(lst)/n))
 lst.sort()
 print(lst[n//2])
 
-cnt = dict()
-for i in lst:
-    if i in cnt:
-        cnt[i] += 1
-    else:
-        cnt[i] = 1
-mx = max(cnt.values())
-mxlst = []
-for i in cnt:
-    if cnt[i] == mx:
-        mxlst.append(i)
-mxlst.sort()
-if len(mxlst) == 1:
-    print(mxlst[0])
+from collections import Counter
+cnt = Counter(lst)
+mode = cnt.most_common()
+if len(mode)==1 or mode[0][1] != mode[1][1]:
+    print(mode[0][0])
 else:
-    print(mxlst[1])
+    print(mode[1][0])
 
 print(max(lst)-min(lst))
