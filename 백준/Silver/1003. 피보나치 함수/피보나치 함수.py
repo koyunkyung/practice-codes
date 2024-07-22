caeeee730dafe9f27f73cef3486705cd73f58d89
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-22 12:25:49 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,14 +1,8 @@
-zero = [1,0,1]
-one = [0,1,1]
-def fibonacci(num):
-    length = len(zero)
-    if num >= length:
-        for i in range(length, num+1):
-            zero.append(zero[i-1] + zero[i-2])
-            one.append(one[i-1] + one[i-2])
-    print(f"{zero[num]} {one[num]}")
-
 import sys
 t = int(sys.stdin.readline())
 for _ in range(t):
-    fibonacci(int(sys.stdin.readline()))
+    n = int(sys.stdin.readline())
+    zero, one = 1, 0
+    for i in range(n):
+        zero, one = one, zero+one
+    print(zero, one)
