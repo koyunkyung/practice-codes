b1d780e3ce99eaf975e564f3e12489be1f6761e8
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-25 15:19:25 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,29 +1,35 @@
-# DFS 풀이
+# BFS 풀이
 
 import sys
-sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-def dfs(x,y):
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
+T = int(input())
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def BFS(x,y):
+    queue = [(x,y)]
+    matrix[x][y] = 0
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if (0<=nx<n) and (0<=ny<m):
-            if field[nx][ny] == 1:
-                field[nx][ny] = -1
-                dfs(nx,ny)
-for _ in range(int(sys.stdin.readline())):
-    m,n,k = map(int, sys.stdin.readline().split())
-    field = [[0]*m for _ in range(n)]
+    while queue:
+        x,y = queue.pop(0)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            if matrix[nx][ny] == 1:
+                queue.append((nx,ny))
+                matrix[nx][ny] = 0
+for i in range(T):
+    m,n,k = map(int, input().split())
+    matrix = [[0]*n for _ in range(m)]
     cnt = 0
-    for _ in range(k):
-        x,y = map(int, sys.stdin.readline().split())
-        field[y][x] = 1
-    for x in range(n):
-        for y in range(m):
-            if field[x][y] > 0:
-                dfs(x,y)
+    for j in range(k):
+        x,y = map(int, input().split())
+        matrix[x][y] = 1
+    for a in range(m):
+        for b in range(n):
+            if matrix[a][b] == 1:
+                BFS(a,b)
                 cnt += 1
     print(cnt)
