1f39ecc57893f4cce53c86f02ca5df358f429697
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-25 16:19:23 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -2,28 +2,22 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-white = 0
-blue = 0
-def valid(arr):
-    global white
-    global blue
-    l = len(arr)
-    total = l**2
-
-    s = 0
-    for row in arr:
-        s += sum(r for r in row)
-    if s == 0 or s == total:
-        if arr[0][0]:
-            blue += 1
-        else:
-            white += 1
+paper = [list(map(int, input().split())) for _ in range(n)]
+result = []
+def solution(x,y,n):
+    color = paper[x][y]
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if color != paper[i][j]:
+                solution(x,y,n//2)
+                solution(x,y+n//2,n//2)
+                solution(x+n//2,y,n//2)
+                solution(x+n//2,y+n//2,n//2)
+                return
+    if color == 0:
+        result.append(0)
     else:
-        valid([a[:l//2] for a in arr[:l//2]])
-        valid([a[l//2:] for a in arr[:l//2]])
-        valid([a[:l//2] for a in arr[l//2:]])
-        valid([a[l//2:] for a in arr[l//2:]])
-valid(arr)
-print(white)
-print(blue)
+        result.append(1)
+solution(0,0,n)
+print(result.count(0))
+print(result.count(1))
