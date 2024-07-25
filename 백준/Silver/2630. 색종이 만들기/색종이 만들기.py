675bccc76ba40bc51446dc065be2ab162b60d2b3
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-25 16:14:02 +0900

    [Silver II] Title: 색종이 만들기, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+white = 0
+blue = 0
+def valid(arr):
+    global white
+    global blue
+    l = len(arr)
+    total = l**2
+
+    s = 0
+    for row in arr:
+        s += sum(r for r in row)
+    if s == 0 or s == total:
+        if arr[0][0]:
+            blue += 1
+        else:
+            white += 1
+    else:
+        valid([a[:l//2] for a in arr[:l//2]])
+        valid([a[l//2:] for a in arr[:l//2]])
+        valid([a[:l//2] for a in arr[l//2:]])
+        valid([a[l//2:] for a in arr[l//2:]])
+valid(arr)
+print(white)
+print(blue)
