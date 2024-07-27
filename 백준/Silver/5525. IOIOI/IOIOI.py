998977a057366bb4633527567c4e1486aea1f0b0
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-27 19:21:37 +0900

    [Silver I] Title: IOIOI, Time: 420 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,13 +2,18 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 m = int(input())
-s = input().strip()
+s = input()
 
-p = 'IO'*n + 'I'
-cnt = 0
-for i in range(m):
-    if s[i] == 'I':
-        if s[i:i+(2*n+1)] == p:
-            cnt += 1
+cursor, cnt, result = 0, 0, 0
+while cursor < (m-1):
+    if s[cursor : cursor+3] == 'IOI':
+        cnt += 1
+        cursor += 2
+        if cnt == n:
+            result += 1
+            cnt -= 1
+    else:
+        cursor += 1
+        cnt = 0
 
-print(cnt)
+print(result)
