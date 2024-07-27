4c8fa513288fad9f596e483adf3f1cb942bab16c
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-27 18:52:31 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,16 +5,18 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
-queue = deque([(0,0)])
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
-cnt = 0
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        next_x, next_y = x+dx[i], y+dy[i]
-        if 0 <= next_x < n and 0 <= next_y < m:
-            if graph[next_x][next_y] == 1:
-                queue.append((next_x, next_y))
-                graph[next_x][next_y] = graph[x][y] + 1
-print(graph[n-1][m-1])
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+    return graph[n-1][m-1]
+print(bfs(0,0))
