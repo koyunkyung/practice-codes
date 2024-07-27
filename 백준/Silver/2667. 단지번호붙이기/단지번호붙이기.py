99ad1c4fa68acd684ab9d308faa951614456dbf4
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-27 19:07:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,4 +1,4 @@
-# DFS
+# BFS
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -6,30 +6,35 @@ input = sys.stdin.readline
 n = int(input())
 graph = []
 result = []
-cnt = 0
 for _ in range(n):
     graph.append(list(map(int, input().rstrip())))
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
-def dfs(x, y):
-    global cnt
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return
-    if graph[x][y] == 1:
-        cnt += 1
+def bfs(graph, a, b):
+    queue = deque()
+    queue.append([a, b])
+    graph[a][b] = 0
+    cnt = 1
+    while queue:
+        x, y = queue.popleft()
         graph[x][y] = 0
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            dfs(nx, ny)
+            if nx < 0 or nx >= len(graph) or ny < 0 or ny >= len(graph):
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append([nx, ny])
+                cnt += 1
+    return cnt
 
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            dfs(i, j)
+            cnt = bfs(graph, i, j)
             result.append(cnt)
-            cnt = 0
 result.sort()
 print(len(result))
 for k in result:
