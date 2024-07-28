f6afd122ddf06b9efc07131b327b51eaa9bbfd5c
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-07-28 22:19:49 +0900

    [Silver I] Title: 경로 찾기, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,24 +1,22 @@
-# BFS
-from collections import deque
+# DFS
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0]*n for _ in range(n)]
+visited = [0 for _ in range(n)]
 
-def bfs(x):
-    queue = deque()
-    queue.append(x)
-    check = [0 for _ in range(n)]
-    while queue:
-        q = queue.popleft()
-        for i in range(n):
-            if check[i] == 0 and graph[q][i] == 1:
-                queue.append(i)
-                check[i] = 1
-                visited[x][i] = 1
-for i in range(0, n):
-    bfs(i)
-for i in visited:
-    print(*i)
+def dfs(x):
+    for i in range(n):
+        if graph[x][i] == 1 and visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+for i in range(n):
+    dfs(i)
+    for j in range(n):
+        if visited[j] == 1:
+            print(1, end = ' ')
+        else:
+            print(0, end = ' ')
+    print()
+    visited = [0 for _ in range(n)]
