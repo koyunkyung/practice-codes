1bd18e35dbdb8b2b4c3c121941bcb303b661d626
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-07 17:02:35 +0900

    [Gold IV] Title: 트리의 지름, Time: 68 ms, Memory: 34552 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,7 +1,7 @@
-# DFS
+# BFS
+from collections import deque
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
 n = int(input())
 tree = [[] for _ in range(n+1)]
@@ -10,21 +10,19 @@ for _ in range(n-1):
     tree[p].append((c,d))
     tree[c].append((p,d))
 
-visited = [-1]*(n+1)
+def bfs(start):
+    visited = [-1]*(n+1)
+    visited[start] = 0
+    queue = deque()
+    queue.append(start)
 
-def dfs(start, distance):
-    for next, next_d in tree[start]:
-        if visited[next] == -1:
-            visited[next] = distance + next_d
-            dfs(next, distance+next_d)
+    while queue:
+        cur = queue.popleft()
+        for next, next_d in tree[cur]:
+            if visited[next] == -1:
+                queue.append(next)
+                visited[next] = visited[cur] + next_d
+    m = max(visited)
+    return[visited.index(m), m]
 
-# 시작 정점에서 임의의 정점까지의 거리를 구하고 그 중 가장 먼 거리를 구한다.
-visited[1] = 0
-dfs(1,0)
-# 위에서 찾은 노드에 대한 가장 먼 노드를 찾고, 가장 먼 노드를 시작지점으로 하여 다시 한번 가장 긴 거리를 찾는다.
-last_Node = visited.index(max(visited))
-visited = [-1]*(n+1)
-visited[last_Node] = 0
-dfs(last_Node, 0)
-
-print(max(visited))
+print(bfs(bfs(1)[0])[1])
