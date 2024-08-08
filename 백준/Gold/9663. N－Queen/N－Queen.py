03f286b535a43e5b7ba6a7d477ad7616cd4cab4e
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-08 15:19:05 +0900

    [Gold IV] Title: N-Queen, Time: 29296 ms, Memory: 198964 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -3,31 +3,25 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-maps = [[0]*n for _ in range(n)]
-used_c = [False]*n  # 열 점유 여부
-used_up = [False]*(2*(n-1)+1)   # y=x 선상 점유 여부
-used_down = [False]*(2*(n-1)+1) # y=-x 선상 점유 여부
-cnt = 0
+ans = 0
+row = [0]*n
 
-def sol(k): # k: 놓은 말 개수 - 점유 행 번호
-    # 퀸: 상하좌우-x,y좌표 확인 / 대각선-(y=x선상:used_up) r+c 같으면, (y=-x선상:used_down) r-c 같으면 놓을 수 없음
-    global n, cnt
+def is_promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x]-row[i]) == abs(x-i):
+            return False
+    return True
 
-    if k == n:
-        cnt += 1
+def n_queens(x):
+    global ans
+    if x == n:
+        ans += 1
         return
-    
-    # i: 점유 열 번호
-    for i in range(n):
-        if not used_c[i] and not used_up[k+i] and not used_down[(n-1)+k-i]:
-            used_c[i] = True
-            used_up[k+i] = True
-            used_down[(n-1)+k-i] = True
+    else:
+        for i in range(n):
+            row[x] = i  # [x,i]에 퀸을 놓겠다
+            if is_promising(x):
+                n_queens(x+1)
 
-            sol(k+1)
-            used_c[i] = False
-            used_up[k+i] = False
-            used_down[(n-1)+k-i] = False
-
-sol(0)
-print(cnt)
+n_queens(0)
+print(ans)
