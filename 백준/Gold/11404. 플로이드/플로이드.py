bfe0ab73f2a207dd91bc4e5912f8f9f64827b016
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-09 14:41:42 +0900

    [Gold IV] Title: 플로이드, Time: 532 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,31 @@
+# 플로이드-와샬 알고리즘 (노드 사이 거리 중 최솟값)
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+n = int(input())
+m = int(input())
+bus = []
+cost = [[INF for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            cost[i][j] = 0
+for i in range(m):
+    a, b, c = map(int, input().split())
+    if cost[a][b] > c:
+        cost[a][b] = c
+
+for k in range(n+1):
+    for i in range(n+1):
+        for j in range(n+1):
+            cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if cost[i][j] == int(1e9):
+            print(0, end=' ')
+        else:
+            print(cost[i][j], end=' ')
+    print()
