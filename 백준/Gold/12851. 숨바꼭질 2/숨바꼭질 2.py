cb682b522430837712ddb44400515692cfa6ddaa
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-09 14:53:49 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 348 ms, Memory: 37624 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,26 +1,27 @@
 # BFS
 import sys
-from collections import deque
+from collections import deque, defaultdict
 input = sys.stdin.readline
-
 n, k = map(int, input().split())
-queue = deque()
-queue.append(n)
-way = [0]*100001
-cnt, result = 0, 0
-while queue:
-    a = queue.popleft()
-    temp = way[a]
-    if a == k:
-        result = temp
-        cnt += 1
-        continue
 
-    for i in [a-1, a+1, 2*a]:
-        # 범위 안에 있고 방문하지 않았거나, 다음 방문이 이전 방문+1이면
-        if 0 <= i < 100001 and (way[i] == 0 or way[i] == way[a]+1):
-            way[i] = way[a] + 1
-            queue.append(i)
+def sol(n, k):
+    min, max = 0, 1e5
+    will_visit = deque([n])
+    visited = [-1]*(int(max+1))
+    visited[n] = 0
+    answer = 0  # 가능한 가짓수
 
-print(result)
-print(cnt)
+    while will_visit:
+        vx = will_visit.popleft()
+        if vx == k:
+            answer += 1
+        
+        for nx in [vx+1, vx-1, 2*vx]:
+            if min <= nx <= max and (visited[nx] == visited[vx]+1 or visited[nx] == -1):
+                will_visit.append(nx)
+                visited[nx] = visited[vx] + 1
+    return visited[k], answer
+
+t, m = sol(n, k)
+print(t)
+print(m)
