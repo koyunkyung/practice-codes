bd0cc5aadaef757b89ccbb0143900ea84881c8ee
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-14 20:14:44 +0900

    [Gold V] Title: 트리와 쿼리, Time: 344 ms, Memory: 66492 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,27 +1,25 @@
-# dfs 알고리즘
+# dfs 탐색 알고리즘
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 n, r, q = map(int, input().split())
-
-m = [[] for _ in range(n+1)]
-visit = [-1 for _ in range(n+1)]
+tree = [[] for _ in range(n+1)]
+count = [0] * (n+1)
 
 for _ in range(n-1):
     a, b = map(int, sys.stdin.readline().split())
-    m[a].append(b)
-    m[b].append(a)
+    tree[a].append(b)
+    tree[b].append(a)
 
-def dfs(now):
-    global visit
-    visit[now] = 1
-    for i in m[now]:
-        if visit[i] == -1:     # 방문하지 않은 방문 가능 노드가 있다면
-            visit[now] += dfs(i)    # 방문하며 그 노드의 서브트리 개수를 더해준다
-    return visit[now]   # 내 서브트리 개수 리턴
+def countPoint(x):
+    count[x] = 1
+    for i in tree[x]:
+        if not count[i]:
+            countPoint(i)
+            count[x] += count[i]
 
-dfs(r)
-for _ in range(q):
-    get = int(input())
-    print(visit[get])
+countPoint(r)
+for i in range(q):
+    u = int(input())
+    print(count[u])
