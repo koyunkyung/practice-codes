4918885e459f975f23cbbb1e378d28a945c93ff8
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-14 19:47:47 +0900

    [Gold V] Title: 용액, Time: 104 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,31 @@
+# 투포인터 알고리즘
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+liquids = list(map(int, input().split()))
+
+left_idx = 0
+right_idx = n-1
+
+ans = abs(liquids[left_idx] + liquids[right_idx])
+ans_left = left_idx
+ans_right = right_idx
+
+while left_idx < right_idx:
+    tmp = liquids[left_idx] + liquids[right_idx]
+
+    if abs(tmp) < ans:
+        ans_left = left_idx
+        ans_right = right_idx
+        ans = abs(tmp)
+        if ans == 0:
+            break
+    
+    if tmp < 0:
+        left_idx += 1
+    
+    else:
+        right_idx -= 1
+
+print(liquids[ans_left], liquids[ans_right])
