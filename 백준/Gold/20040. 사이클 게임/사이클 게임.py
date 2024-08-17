5e475413ec6233f24c182ae6577b769b322e0e52
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-17 12:03:07 +0900

    [Gold IV] Title: 사이클 게임, Time: 580 ms, Memory: 62872 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -1,28 +1,38 @@
 # union-find 알고리즘
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
 
 n, m = map(int, input().split())
-parent = [x for x in range(n)]
+root = [i for i in range(n)]
+rank = [0 for _ in range(n)]
+conn = [0 for _ in range(m)]
 
-def find(x):
-    while x != parent[x]:
-        x = parent[x]
-    return x
+def find(v):
+    if v == root[v]:
+        return v
+    root[v] = find(root[v])
+    return root[v]
 
-def union(x, y):
-    parent_x = find(x)
-    parent_y = find(y)
-    if parent_x < parent_y:
-        parent[parent_y] = parent_x
+def union(v1, v2):
+    r1 = find(v1)
+    r2 = find(v2)
+
+    if rank[r1] > rank[r2]:
+        root[r2] = r1
+    elif rank[r1] < rank[r2]:
+        root[r1] = r2
     else:
-        parent[parent_x] = parent_y
+        root[r2] = r1
+        rank[r1] += 1
 
-result = 0
-for i in range(1, m+1):
-    a, b = map(int, input().split())
-    if find(a) == find(b):
-        result = i
-        break
-    union(a, b)
-print(result)
+def solve():
+    for i in range(m):
+        a, b = map(int, input().split())
+        if find(a) == find(b):
+            print(i+1)
+            return
+        union(a, b)
+    print(0)
+    return
+solve()
