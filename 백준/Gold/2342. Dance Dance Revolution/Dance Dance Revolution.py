33c8b2c16b48ada327ec64d7c892cf7ba922c215
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-18 16:38:09 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 3760 ms, Memory: 188468 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -1,52 +1,24 @@
-# dp 알고리즘
-# dp[i][l][r] = i번째 움직임을 (l,r)의 발 위치로 수행했을 때 드는 힘의 총합
-import sys
-sys.setrecursionlimit(10**8)
-input = lambda: sys.stdin.readline().rstrip()
+nums = [*map(int, input().split())]
 
-max = 400000
+# dp[i][left][right] = minimum of sum of power - [ith][left foot][right foot]
+dp = [[[4*len(nums) for _ in range(5)] for _ in range(5)] for _ in range(len(nums))]
+dp[0][0][0] = 0
 
-# 어떤 발이 k -> lr로 갈 때 드는 비용
-def get_add(lr, k):
-    if k == 0:
-        if lr == 0:
-            return 0
-        else:
-            return 2
-    elif k == lr:
+def po(a, b):
+    if a == 0:
+        return 2
+    elif a == b:
         return 1
-    elif abs(k-lr) == 1 or abs(k-lr) == 3:
-        return 3
-    else:
+    elif abs(a-b) == 2:
         return 4
+    else:
+        return 3
 
-move = list(map(int, input().split()))
-move.pop()
-n = len(move)
-if n == 0:
-    print(0)
-    exit()
+for i in range(len(dp)-1):
+    a = nums[i]
+    for left in range(5):
+        for right in range(5):
+            dp[i+1][left][a] = min(dp[i+1][left][a], dp[i][left][right]+po(right, a))
+            dp[i+1][a][right] = min(dp[i+1][a][right], dp[i][left][right]+po(left, a))
 
-dp = [[[max+1 for _ in range(5)] for _ in range(5)] for _ in range(n+1)]
-dp[-1][0][0] = 0
-
-for i in range(n):
-    # l = move[i], 왼발로 이번 위치 누를 때, 즉 이번에 왼발이 움직일 것
-    # 왼발이 움직이니 오른발은 고정. k는 왼발의 이전 위치.
-    for r in range(5):
-        for k in range(5):
-            add = get_add(move[i], k)
-            dp[i][move[i]][r] = min(dp[i][move[i]][r], dp[i-1][k][r]+add)
-    
-    # r = move[i], 오른발로 이번 위치 누를 때, 즉 이번에 오른발이 움직일 것
-    # 오른발이 움직이니 왼발은 고정. k는 오른발의 이전 위치.
-    for l in range(5):
-        for k in range(5):
-            add = get_add(move[i], k)
-            dp[i][l][move[i]] = min(dp[i][l][move[i]], dp[i-1][l][k]+add)
-
-m = max+1
-for l in range(5):
-    for r in range(5):
-        m = min(m, dp[n-1][l][r])
-print(m)
+print(min(map(min, dp[len(dp)-1])))
