0d52eedbc14ae89ae2d20f2d32208f1be83974d4
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-21 03:54:54 +0900

    [Gold II] Title: 1의 개수 세기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
@@ -1,19 +1,24 @@
 import sys
-import math
-sys.setrecursionlimit(10**8)
-input = lambda: sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
-a, b = map(int, input().split())
+x, y = map(int, input().split())
+one_sum = [0 for _ in range(60)]
 
-def sum_f(x):
-    if x <= 0:
-        return 0
-    seung = int(math.log2(x))
-    floor_2pow = 2 ** seung
-    if floor_2pow == x:
-        return seung * x // 2 + 1
-    
-    diff = x - floor_2pow
-    return sum_f(floor_2pow) + diff + sum_f(diff)
+def count(num):
+    cnt = 0
+    bin_num = bin(num)[2:]
+    length = len(bin_num)
+    for i in range(length):
+        if bin_num[i] == '1':
+            # num보다 크지 않으면서 가장 큰 2의 거듭제곱수
+            val = length - i - 1
+            cnt += one_sum[val]
+            # 가장 앞자리 1 개수를 추가로 더해주기
+            cnt += (num - 2**val + 1)
+            num = num - 2**val
+    return cnt
 
-print(sum_f(b) - sum_f(a-1))
+for i in range(1, 60):
+    one_sum[i] = 2**(i-1) + 2*one_sum[i-1]
+
+print(count(y) - count(x-1))
