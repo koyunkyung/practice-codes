9beac7cf165a3d7faa06d72d283513dea492c8ed
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-21 01:28:33 +0900

    [Gold II] Title: 문제집, Time: 164 ms, Memory: 42468 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,29 @@
+# 위상정렬
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+answer = []
+graph = [[] for _ in range(n+1)]
+inDegree = [0 for _ in range(n+1)]
+queue = []
+
+for i in range(m):
+    first, second = map(int, input().split())
+    graph[first].append(second)
+    inDegree[second] += 1
+
+for i in range(1, n+1):
+    if inDegree[i] == 0:
+        heapq.heappush(queue, i)
+
+while queue:
+    tmp = heapq.heappop(queue)
+    answer.append(tmp)
+    for i in graph[tmp]:
+        inDegree[i] -= 1
+        if inDegree[i] == 0:
+            heapq.heappush(queue, i)
+
+print(' '.join(map(str, answer)))
