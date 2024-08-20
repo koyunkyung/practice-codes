ef74b258c657158213e6c00f5abe99e751961643
koyunkyung <133300121+koyunkyung@users.noreply.github.com>
2024-08-21 04:30:50 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 812 ms, Memory: 145332 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,5 +1,6 @@
-# 이분탐색
+# bisect 모듈 활용
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
 
 n = int(input())
@@ -7,25 +8,11 @@ a = [*map(int, input().split())]
 
 lis = [a[0]]
 
-def findPlace(e):
-    start = 0
-    end = len(lis)-1
-    while start <= end:
-        mid = (start+end) // 2
-
-        if lis[mid] == e:
-            return mid
-        elif lis[mid] < e:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return start
-
 for item in a:
     if lis[-1] < item:
         lis.append(item)
     else:
-        idx = findPlace(item)
+        idx = bisect_left(lis, item)
         lis[idx] = item
 
 print(len(lis))
